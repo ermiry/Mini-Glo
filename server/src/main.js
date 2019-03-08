3d9d2e04f980a7bdd9e4191c7d1bb6d8e593c2ea
ermiry <stumpestro@ermiry.com>
2019-03-07 23:48:13 -0600

    Setting up express

---
+++ server/src/main.js
@@ -0,0 +1,41 @@
+const express = require ('express');
+// const mongoose = require ('mongoose');
+const bodyParser = require ('body-parser');
+// const passport = require ('passport');
+
+const app = express ();
+
+// Body parser middleware
+app.use (bodyParser.urlencoded ({ extended: false }));
+app.use (bodyParser.json ());
+
+// DB Config
+// TODO:
+// const db = require('./config/keys').mongoURI;
+
+// Connect to MongoDB
+/* mongoose
+    .connect ('mongodb://localhost/test', { useNewUrlParser: true })
+    .then (() => console.log ('MongoDB Connected'))
+    .catch (err => console.log (err)); */
+
+// passport
+// app.use (passport.initialize ());
+// require ('../config/passport')(passport);
+
+// serve static files from our public dir
+// app.use (express.static ("./public"));
+
+/*** ROUTES ***/
+
+// app.use ('/api/users', users);
+
+// projects api routes
+// app.use ('/api/', projects);
+
+// catch all, for now redirect to the home page
+// app.get ("*", (req, res) => { res.redirect ("/"); });
+
+const port = process.env.PORT || 8080;
+
+app.listen (port, () => console.log (`Server running on port ${port}`));
