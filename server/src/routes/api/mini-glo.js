3e52e98e5cd0bab307f88aad6532a4b67aef262c
Erick Salas <stumpestro@ermiry.com>
2019-03-12 14:03:42 -0600

    Added test post route

--- server/src/routes/api/mini-glo.js
+++ server/src/routes/api/mini-glo.js
@@ -6,4 +6,17 @@ const router = express.Router ();
 // @access  Public
 router.get ('/test', (req, res) => res.json ({ msg: 'Mini-Glo Works' }));
 
+// @route   POST api/mini-glo/test
+// @desc    Tests mini glo route
+// @access  Public
+router.post ('/test', (req, res) => {
+
+    let msg = req.body.msg;
+
+    console.log (msg);
+
+    res.json ({ status: 'Success' });
+
+});
+
 module.exports = router;
