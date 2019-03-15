683c6d2d2465ffbe20a3bc9cd369ee2cf40a1040
Erick Salas <stumpestro@ermiry.com>
2019-03-15 17:26:41 -0600

    Added oauth route

--- server/src/routes/api/mini-glo.js
+++ server/src/routes/api/mini-glo.js
@@ -43,5 +43,37 @@ router.post ('/test', (req, res) => {
 // @desc    Deletes a board by id
 // @access  Private
 
+// https://app.gitkraken.com/oauth/authorize?response_type=code&client_id=b3rb17pn8k6y4z9hkyu4&redirect_uri=http://localhost:10000/api/mini-glo/oauth&scope=board%3Aread%20board%3Awrite%20user%3Aread%20user%3Awrite&state=V2VkIE1hciAxMyAyMDE5IDExOjA1OjAyIEdNVC0wNjAwIChDZW50cmFsIFN0YW5kYXJkIFRpbWUp
+
+router.get('/oauth',(req,res,next)=>{
+    //get the query
+    const{query} = req;
+    // ?code=1254131h151
+    //get the code 
+    const{code} = query;
+    if(!code){
+        return res.send({
+            succes:false,
+            message:'Error:no code'
+        });
+    }  
+    console.log('code',code);
+    //Post
+  
+    request
+    .post('https://api.gitkraken.com/oauth/access_token')
+    .send({ 
+      grant_type: 'authorization_code',
+      client_id:'b3rb17pn8k6y4z9hkyu4',
+      client_secret:'ckn1m4yxi8h64ts7dzk45ybnv35a9ckqriy03rv6',
+      code:code 
+    })
+    .set('Accept', 'application/json')
+    .then(result => {
+       const data = result.body;
+       res.send(data);
+    });
+  });
+
 
 module.exports = router;
