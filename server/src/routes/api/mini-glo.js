a5bc20d053b4cb4c1e329ff8bdc56bcfc88d0020
Unknown <jlaraanaya@gmail.com>
2019-03-19 13:54:04 -0600

    Routes are working

--- server/src/routes/api/mini-glo.js
+++ server/src/routes/api/mini-glo.js
@@ -1,29 +1,27 @@
 const express = require ('express');
 const router = express.Router ();
 const request = require('superagent');
+const app = express();
+const path = require('path');
+var opn = require('opn');
+var querystring = require('querystring');
 var columnName = "", board = "";
+var accessToken;
+var url;
 // @route   GET api/mini-glo/test
 // @desc    Tests mini glo route
 // @access  Public
-router.get ('/test', (req, res) => {
-    console.log("Get Request was made");      
-    res.json (   
-    { msg: "hola ",
-      boardName: 'Mike' 
-    });
+router.get ('/authorize', (req, res) => {
+    console.log("Get Request was made"); 
+    url = req.url;    
+    res.sendFile(path.join(__dirname+'/html/index.html')); 
 });
-
-
-router.post ('/test',(req,res)=>{
-    let columnName = columnName.body.any;
-    console.log(columnName);
-    res.json({status:'Success'});
-});
-
 
 //DO A GET
 router.get('/oauth',(req,res,next)=>{
   //get the query
+  
+  var q = querystring.parse(url);
   const{query} = req;
   // ?code=1254131h151
   //get the code 
@@ -37,7 +35,7 @@ router.get('/oauth',(req,res,next)=>{
   console.log('code',code);
   //Post
   //required URL for the authorization:
-  //app.gitkraken.com/oauth/authorize?response_type=code&client_id=b3rb17pn8k6y4z9hkyu4&redirect_uri=http://localhost:10000/api/mini-glo/oauth&scope=board%3Aread board%3Awrite user%3Aread user%3Awrite&state=VHVlIE1hciAxMiAyMDE5IDIxOjIxOjU1IEdNVC0wNjAwIChDZW50cmFsIFN0YW5kYXJkIFRpbWUp
+//https://app.gitkraken.com/oauth/authorize?response_type=code&client_id=b3rb17pn8k6y4z9hkyu4&redirect_uri=https://18540928.ngrok.io/api/mini-glo/oauth&scope=board%3Aread board%3Awrite user%3Aread user%3Awrite&state=VHVlIE1hciAxMiAyMDE5IDIxOjIxOjU1IEdNVC0wNjAwIChDZW50cmFsIFN0YW5kYXJkIFRpbWUp
   request
   .post('https://api.gitkraken.com/oauth/access_token')
   .send({ 
@@ -49,8 +47,54 @@ router.get('/oauth',(req,res,next)=>{
   .set('Accept', 'application/json')
   .then(result => {
      const data = result.body;
-     res.send(data);
+     var obj = JSON.parse(result.text);
+     accessToken =  obj.access_token;
+     console.log("AccessToken: " + accessToken);
+     //opn('https://pitangui.amazon.com/api/skill/link/M1IE0AW91STLMJ' + '?state=' + q.state + '&code=' + code);
+     res.sendFile(path.join(__dirname+'/html/register.html'));
+  })
+});
+
+router.get('/token',(req,res)=>{
+  console.log("Get token was made");
+  console.log("AcessToken: " + accessToken);
+  res.json({
+    token:accessToken
   });
 });
+
+router.get('/boards',(req,res)=>{
+  console.log("Get Board was made");
+  //const{boardName} = req.boardName;
+  //const{token} = accessToken;
+  request.get('https://gloapi.gitkraken.com/v1/glo/boards')
+  .auth(accessToken,{type:"bearer"})
+  .set('Accept','application/json')
+  .then(result =>{
+    const data = result.body;
+    console.log(result.status + "\n" + result.header + "\n" + data);
+    res.send(data);
+  })
+  .catch(err=>{
+    console.error(err.message);
+  })
+});
+
+router.post('/boardPost',(req,res)=>{
+  console.log("Post Board was made");
+  request.post('https://gloapi.gitkraken.com/v1/glo/boards')
+  .auth(accessToken,{type:"bearer"})
+  .send({
+    name:"ErickEsjoto"
+  })
+  .set('Accept','application/json')
+  .then(result=>{
+    const data = result.body;
+    res.send(data);
+  })
+  .catch(err=>{
+    console.error(err.message);
+  })
+})
 
 module.exports = router;
