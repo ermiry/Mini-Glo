db3a8bfaeee874734651316084e78e802864dea3
Erick Salas <stumpestro@ermiry.com>
2019-03-20 20:47:41 -0600

    Work on api routes

--- server/src/routes/api/mini-glo.js
+++ server/src/routes/api/mini-glo.js
@@ -1,7 +1,8 @@
 const express = require ('express');
 const router = express.Router ();
 
-const axios = require('axios');
+const axios = require ('axios');
+const request = require ('superagent');
 
 // @route   GET api/mini-glo/test
 // @desc    Tests mini glo route
@@ -21,7 +22,40 @@ router.post ('/test', (req, res) => {
 
 });
 
-/* FIXME: can we use passport outh?? */
+// @route   GET api/mini-glo/oauth
+// @desc    Handles mini glo oauth
+// @access  Public
+router.get ('/oauth', (req, res) => {
+
+	const { code } = req;
+	if (!code) {
+		res.send ({
+			succes: false,
+			message: 'Error: no code'
+		});
+	}
+
+	// FIXME: move this to a protected file
+	request.post ('https://api.gitkraken.com/oauth/access_token')
+		.set ('Accept', 'application/json')
+		.send ({
+			grant_type: 'authorization_code',
+			client_id: 'b3rb17pn8k6y4z9hkyu4',
+			client_secret: 'ckn1m4yxi8h64ts7dzk45ybnv35a9ckqriy03rv6',
+			code: code 
+		})
+		.then (result => {
+			/*** TODO: check this later ***/
+			res.send (result.body);
+		})
+		.catch (err => {
+			let { errors };
+			console.error (err.message);
+			errors.board = 'Faild oauth.';
+        	return res.status (400).json (errors); 
+		});
+
+});
 
 // @route   GET api/mini-glo/boards
 // @desc    Get a list of boards
@@ -30,50 +64,55 @@ router.post ('/test', (req, res) => {
 // @route   POST api/mini-glo/boards
 // @desc    Creates a new board
 // @access  Private
+router.post ('/boards', (req, res) => {
 
-// @route   GET api/mini-glo/boards/:board_id
+	request.post ('https://gloapi.gitkraken.com/v1/glo/boards')
+		.auth (req.body.token, { type: "bearer"})
+		.set ('Accept', 'application/json')
+		.send ({ name: req.body.boardName })
+		.then (result => {
+			// res.send (result);
+			let { board };
+			board.id = result.body.id;
+			board.name = result.body.name;
+			return res.status (200).json (board);
+		})
+		.catch (err => {
+			let { errors };
+			console.error (err.message);
+			errors.board = 'Failed to create board.';
+        	return res.status (400).json (errors); 
+		});
+
+});
+
+/***** */
+// TODO: req.body.id!!!
+/***** */
+
+// @route   GET api/mini-glo/boards/board_id
 // @desc    Gets a board by ID
 // @access  Private
 
-// @route   POST api/mini-glo/boards/:board_id
+// @route   POST api/mini-glo/boards/board_id
 // @desc    Edits a board by id
 // @access  Private
 
-// @route   DELETE api/mini-glo/boards/:board_id
+// @route   DELETE api/mini-glo/boards/board_id
 // @desc    Deletes a board by id
 // @access  Private
 
-// https://app.gitkraken.com/oauth/authorize?response_type=code&client_id=b3rb17pn8k6y4z9hkyu4&redirect_uri=http://localhost:10000/api/mini-glo/oauth&scope=board%3Aread%20board%3Awrite%20user%3Aread%20user%3Awrite&state=V2VkIE1hciAxMyAyMDE5IDExOjA1OjAyIEdNVC0wNjAwIChDZW50cmFsIFN0YW5kYXJkIFRpbWUp
+// @route   POST /boards/board_id/columns
+// @desc    Creates a new column in a board
+// @access  Private
 
-router.get('/oauth',(req,res,next)=>{
-    //get the query
-    const{query} = req;
-    // ?code=1254131h151
-    //get the code 
-    const{code} = query;
-    if(!code){
-        return res.send({
-            succes:false,
-            message:'Error:no code'
-        });
-    }  
-    console.log('code',code);
-    //Post
-  
-    request
-    .post('https://api.gitkraken.com/oauth/access_token')
-    .send({ 
-      grant_type: 'authorization_code',
-      client_id:'b3rb17pn8k6y4z9hkyu4',
-      client_secret:'ckn1m4yxi8h64ts7dzk45ybnv35a9ckqriy03rv6',
-      code:code 
-    })
-    .set('Accept', 'application/json')
-    .then(result => {
-       const data = result.body;
-       res.send(data);
-    });
-  });
+// @route   DELETE /boards/board_id/columns/column_id
+// @desc    Deletes a column by id
+// @access  Private
+router.delete ('/boards/board_id/columns/column_id', (req, res) => {
 
+	
+
+});
 
 module.exports = router;
