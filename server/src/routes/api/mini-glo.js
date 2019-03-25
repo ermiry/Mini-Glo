64611f95964350f720a52d9496f7b39926753460
Erick Salas <stumpestro@ermiry.com>
2019-03-25 08:14:33 -0600

    Created routes templates

--- server/src/routes/api/mini-glo.js
+++ server/src/routes/api/mini-glo.js
@@ -4,6 +4,8 @@ const router = express.Router ();
 const axios = require ('axios');
 const request = require ('superagent');
 
+/*** TEST ***/
+
 // @route   GET api/mini-glo/test
 // @desc    Tests mini glo route
 // @access  Public
@@ -21,6 +23,8 @@ router.post ('/test', (req, res) => {
     res.json ({ status: 'Success' });
 
 });
+
+/*** OAUTH ***/
 
 // @route   GET api/mini-glo/oauth
 // @desc    Handles mini glo oauth
@@ -57,11 +61,16 @@ router.get ('/oauth', (req, res) => {
 
 });
 
-// @route   GET api/mini-glo/boards
+/*** BOARDS ***/
+
+// @route   GET /boards
 // @desc    Get a list of boards
 // @access  Private
+router.get ('/boards', (req, res) => {
 
-// @route   POST api/mini-glo/boards
+});
+
+// @route   POST /boards
 // @desc    Creates a new board
 // @access  Private
 router.post ('/boards', (req, res) => {
@@ -90,29 +99,133 @@ router.post ('/boards', (req, res) => {
 // TODO: req.body.id!!!
 /***** */
 
-// @route   GET api/mini-glo/boards/board_id
+// @route   GET /boards/board_id
 // @desc    Gets a board by ID
 // @access  Private
+router.get ('/boards/board_id', (req, res) => {
 
-// @route   POST api/mini-glo/boards/board_id
+});
+
+// @route   POST /boards/board_id
 // @desc    Edits a board by id
 // @access  Private
+router.post ('/boards/board_id', (req, res) => {
 
-// @route   DELETE api/mini-glo/boards/board_id
+});
+
+// @route   DELETE /boards/board_id
 // @desc    Deletes a board by id
 // @access  Private
+router.delete ('/boards/board_id', (req, res) => {
+
+});
+
+/*** COLUMNS ***/
 
 // @route   POST /boards/board_id/columns
 // @desc    Creates a new column in a board
-// @access  Private
+router.post ('/boards/board_id/columns', (req, res) => {
+
+});
+
+// @route   POST /boards/board_id/columns/column_id
+// @desc    Edits a column by id
+router.delete ('/boards/board_id/columns/column_id', (req, res) => {
+
+});
 
 // @route   DELETE /boards/board_id/columns/column_id
 // @desc    Deletes a column by id
-// @access  Private
 router.delete ('/boards/board_id/columns/column_id', (req, res) => {
 
-	
+});
+
+
+/*** CARDS ***/
+
+// @route   GET /boards/{board_id}/cards
+// @desc    Gets a list of cards for a board
+router.get ('/boards/board_id/cards', (req, res) => {
 
 });
+
+// @route   POST /boards/{board_id}/cards
+// @desc    Creates a new card in a column
+router.post ('/boards/board_id/cards', (req, res) => {
+
+});
+
+// @route   GET /boards/{board_id}/cards/{card_id}
+// @desc    Gets a card by ID
+router.get ('/boards/board_id/cards/card_id', (req, res) => {
+
+});
+
+// @route   POST /boards/{board_id}/cards/{card_id}
+// @desc    Edits a card
+router.post ('/boards/board_id/cards/card_id', (req, res) => {
+
+});
+
+// @route   DELETE /boards/{board_id}/cards/{card_id}
+// @desc    Deletes a card
+router.delete ('/boards/board_id/cards/card_id', (req, res) => {
+
+});
+
+// @route   GET /boards/{board_id}/columns/{column_id}/cards
+// @desc    Gets a list of cards for a column
+router.get ('/boards/board_id/columns/column_id/cards', (req, res) => {
+
+});
+
+/*** ATTACHMENTS ***/
+
+// @route   GET /boards/{board_id}/cards/{card_id}/attachments
+// @desc    Gets a list of attachments for a card
+router.get ('/boards/board_id/cards/card_id/attachments', (req, res) => {
+
+});
+
+// @route   POST /boards/{board_id}/cards/{card_id}/attachments
+// @desc    Creates an attachment for a card
+router.post ('/boards/board_id/cards/card_id/attachments', (req, res) => {
+
+});
+
+/*** COMMENTS ***/
+
+// @route   GET /boards/{board_id}/cards/{card_id}/comments
+// @desc    Gets a list of comments for a card
+router.get ('/boards/board_id/cards/card_id/comments', (req, res) => {
+
+});
+
+// @route   POST /boards/{board_id}/cards/{card_id}/comments
+// @desc    Creates a new comment in a card
+router.post ('/boards/board_id/cards/card_id/comments', (req, res) => {
+
+});
+
+// @route   POST /boards/{board_id}/cards/{card_id}/comments/{comment_id}
+// @desc    Edits a comment by ID
+router.post ('/boards/board_id/cards/card_id/comments/comment_id', (req, res) => {
+
+});
+
+// @route   POST /boards/{board_id}/cards/{card_id}/comments/{comment_id}
+// @desc    Deletes a comment by ID
+router.delete ('/boards/board_id/cards/card_id/comments/comment_id', (req, res) => {
+
+});
+
+/*** USERS ***/
+
+// @route   GET /user
+// @desc    Gets data about the authenticated user
+router.get ('/user', (req, res) => {
+
+});
+
 
 module.exports = router;
